4da071e3a45cef356000fe54377c8e7770559dc2
melank <masaki.vr@gmail.com>
2021-02-01 23:49:48 +0900

    add abc183/c

--- abc183/c/main.py
+++ abc183/c/main.py
@@ -1,7 +1,15 @@
-import math
+import itertools
 
 n, k = map(int, input().split())
+t = [list(map(int, input().split())) for _ in range(n)]
 
-routes = math.factorial(n)
-# １→各ルートへはそれぞれn-2通り行き方がある
-# 各ルート→1へ戻るにはそれぞれn-2通り行き方がある
+nums = list(range(1, n))
+result = 0
+for index in itertools.permutations(nums):
+    index = [0] + list(index)
+    time = 0
+    for i in range(n):
+        time += t[index[i]][index[(i + 1) % n]]
+    if time == k:
+        result += 1
+print(result)
