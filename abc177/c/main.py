1b7035d4cbe4e0afa519cb235ff38db133ac2410
melank <masaki.vr@gmail.com>
2021-02-03 10:05:18 +0900

    add c problem.

--- abc177/c/main.py
+++ abc177/c/main.py
@@ -1,11 +1,15 @@
+mod_ = 1000000007
 n = int(input())
 values = list(map(int, input().split()))
-mod_ = 1000000007
+diffs = [0]
+for i in range(n):
+    diffs.append(diffs[i] + values[i])
+
+result = 0
 sum_ = 0
-for i in range(n - 1):
-    for j in range(i + 1, n):
-        # print(values[i], values[j])
-        if sum_ < mod_:
-            sum_ = mod_ - sum_
-        sum_ += (values[i] * values[j]) % mod_
-print(sum_)
+for i in range(n):
+    sum_ = (diffs[n] - diffs[i + 1]) % mod_
+    result += values[i] * sum_
+    result %= mod_
+
+print(result)
--- abc189/c/main.py
+++ abc189/c/main.py
@@ -1,7 +1,11 @@
 n = int(input())
-mikans = list(map(int, input().split()))
-test = {}
-for m in mikans:
+values = list(map(int, input().split()))
 
+result = 0
+for l in range(n):
+    x = values[l]
+    for r in range(l, n):
+        x = min(x, values[r])
+        result = max(result, x * (r - l + 1))
 
-print(dp)
+print(result)
