806822db932b3fd1a78058fcda0c648522c72e3d
melank <masaki.vr@gmail.com>
2021-02-18 12:54:15 +0900

    abc154/d

--- abc154/d/main.py
+++ abc154/d/main.py
@@ -1,10 +1,19 @@
 n, k = map(int, input().split())
 dices = list(map(int, input().split()))
-e = 0
-diffs = []
-for i in range(n - k - 1):
-    diffs.append(dices[i])
-for d in dices:
-    print("{}'s ex : {}".format(d, sum(list(range(1, d+1))) / d))
-    e += sum(list(range(1, d+1))) * 2 / d
-print(e)
+
+if k == 1:
+    print("{:.12f}".format((1 + max(dices)) / 2))
+    exit()
+
+p = [0] * n
+for i in range(n):
+    p[i] = (dices[i] + 1)
+
+v = sum(p[:k])
+result = v
+
+for j in range(n - k):
+    v = v - p[j] + p[j + k]
+    result = max(v, result)
+
+print("{:.12f}".format(result / 2))
