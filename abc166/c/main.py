d6f18616b16ced13593effe76d67f483c325963f
melank <masaki.vr@gmail.com>
2021-02-20 15:10:20 +0900

    add abc166/c

--- abc166/c/main.py
+++ abc166/c/main.py
@@ -1,2 +1,16 @@
 n, m = map(int, input().split())
 heights = list(map(int, input().split()))
+connection = [True for i in range(n)]
+# print(connection)
+for i in range(m):
+    pa, pb = map(int, input().split())
+    if heights[pa-1] < heights[pb-1]:
+        connection[pa-1] = False
+    elif heights[pb-1] < heights[pa-1]:
+        connection[pb-1] = False
+    else:
+        connection[pa-1] = False
+        connection[pb-1] = False
+    # print(connection)
+
+print(connection.count(True))
