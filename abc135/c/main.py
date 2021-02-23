f9c3fdb2edc00a397be306fed84476e75a42842f
melank <masaki.vr@gmail.com>
2021-02-23 18:25:55 +0900

    add abc135

--- abc135/c/main.py
+++ abc135/c/main.py
@@ -1,9 +1,19 @@
 n = int(input())
 enemies = list(map(int, input().split()))
-power = list(map(int, input().split()))
+powers = list(map(int, input().split()))
 
 total = 0
-for i in range(n):
-    total += min(power[i], enemies[i] + enemies[i + 1])
-    enemies[i + 1] -= min(enemies[i] - power[i], 0)
+for i in range(len(powers)):
+    power = powers[i]
+    first = min(enemies[i], power)
+    if first >= enemies[i]:
+        rest = power - first
+        enemies[i] = 0
+        total += min(enemies[i + 1], rest)
+        enemies[i + 1] = max(0, enemies[i + 1] - rest)
+    else:
+        enemies[i] -= first
+    total += first
+    # print(enemies)
+
 print(total)
