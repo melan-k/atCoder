5ac52619f9de174b4224c808359626a902e5af9e
melank <masaki.vr@gmail.com>
2021-02-26 05:51:21 +0900

    add abc113/b

--- abc113/b/main.py
+++ abc113/b/main.py
@@ -5,8 +5,9 @@ heights = list(map(int, input().split()))
 best = 100000
 index = 0
 for i, h in enumerate(heights):
-    averate = (t - h * 0.006))
-    if abs(a - average) < best:
-        best = abs(a - average)
+    average = (t - h * 0.006)
+    diff = abs(a - average)
+    if diff < best:
+        best = diff
         index = i + 1
-print(i)
+print(index)
