ff1e07aef830a0b4101856499bc9af736f4ea6b8
melank <masaki.vr@gmail.com>
2021-02-26 05:17:54 +0900

    add abc114/b

--- abc113/b/main.py
+++ abc113/b/main.py
@@ -1,3 +1,12 @@
 n = int(input())
 t, a = map(int, input().split())
 heights = list(map(int, input().split()))
+
+best = 100000
+index = 0
+for i, h in enumerate(heights):
+    averate = (t - h * 0.006))
+    if abs(a - average) < best:
+        best = abs(a - average)
+        index = i + 1
+print(i)
