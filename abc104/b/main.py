eda490d75303aaa67046a3f1d8230e44af1cd9c9
melank <masaki.vr@gmail.com>
2021-02-28 17:45:58 +0900

    abc104

---
+++ abc104/b/main.py
@@ -0,0 +1,18 @@
+s = input()
+if s[0] != 'A':
+    print('WA')
+    exit()
+if s[2:-1].count('C') != 1:
+    print('WA')
+    exit()
+
+cnt = 0
+upper = list(['ABCDEFGHIJKLMNOPQRSTUVWXYZ'])
+for c in s:
+    if c in upper:
+        cnt += 1
+
+if cnt != 2:
+    print('WA')
+else:
+    print('AC')
