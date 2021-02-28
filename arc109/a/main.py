eae68a9355c836b139e25fc15f2fcec73af24c46
melank <masaki.vr@gmail.com>
2021-02-28 23:26:14 +0900

    add arc109

--- arc109/a/main.py
+++ arc109/a/main.py
@@ -1,1 +1,14 @@
 a, b, x, y = map(int, input().split())
+
+if a == b:
+    print(x)
+elif a > b:
+    if 2 * x < y:
+        print(x + (a - b - 1) * (2 * x))
+    else:
+        print(x + (a - b - 1) * y)
+else:
+    if 2 * x < y:
+        print(x + (b - a) * (2 * x))
+    else:
+        print(x + (b - a) * y)
