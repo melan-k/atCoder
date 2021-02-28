e99f325ba53881458d82ccde15a4fd250ee515b3
melank <masaki.vr@gmail.com>
2021-02-28 17:11:10 +0900

    agc002

---
+++ agc002/a/main.py
@@ -0,0 +1,11 @@
+a, b = map(int, input().split())
+
+if a < 0 and b >= 0:
+    print('Zero')
+elif 0 < a :
+    print('Positive')
+else:
+    if (b - a) %2 != 0:
+        print('Positive')
+    else:
+        print('Negative')
