5555048eec53faf434f75cb58ca413687ab80c88
melank <masaki.vr@gmail.com>
2021-03-06 01:18:59 +0900

    add abc131

--- abc131/c/main.py
+++ abc131/c/main.py
@@ -1,1 +1,14 @@
+# ユークリッド互除法
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+def num(n, c, d):
+    g = gcd(c, d)
+    l = c // g * d
+    return n - n // c - n // d + n // l
+
 a, b, c, d = map(int, input().split())
+print(int(num(b, c, d) - num(a - 1, c, d)))
