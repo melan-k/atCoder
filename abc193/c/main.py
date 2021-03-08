b0d00c00f11e20db8e09e63aa5805636fa701ae3
melank <masaki.vr@gmail.com>
2021-03-08 22:33:55 +0900

    コメント追加

--- abc193/c/main.py
+++ abc193/c/main.py
@@ -2,8 +2,8 @@ n = int(input())
 sq = int(n ** 0.5)
 s = set()
 for a in range(2, sq + 1):
-    x = a * a
+    x = a ** 2  # 2乗からスタート
     while x <= n:
-        s.add(x)
-        x *= a
+        s.add(x) # 値をsetに登録
+        x *= a # 3乗, 4乗, 5乗と掛け合わせていく
 print(n - len(s))
