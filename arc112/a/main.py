94dbbcb258d7491af1fac87b2a191aaffc146fc9
melank <masaki.vr@gmail.com>
2021-03-08 23:17:14 +0900

    nikkei 2019

--- arc112/a/main.py
+++ arc112/a/main.py
@@ -1,12 +1,1 @@
-import itertools
-
 t = int(input())
-for i in range(t):
-    l, r = map(int, input().split())
-    cnt = 0
-    for comb in itertools.product(set(range(l, r + 1)), repeat=3):
-    #     print(comb)
-        if comb[2] == comb[0] + comb[1]:
-            # print("{} - {} = {}".format(comb[2], comb[1], comb[0]))
-            cnt += 1
-    print(cnt)
