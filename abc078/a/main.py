10eca149c2b6254f6202c6ea971051e4aaf948b6
melank <masaki.vr@gmail.com>
2021-04-30 21:50:53 +0900

    add abc078

---
+++ abc078/a/main.py
@@ -0,0 +1,15 @@
+m = {
+    'A': 1,
+    'B': 2,
+    'C': 3,
+    'D': 4,
+    'E': 5,
+    'F': 6,
+}
+x, y = input().split()
+if m[x] > m[y]:
+    print('>')
+elif m[x] < m[y]:
+    print('<')
+else:
+    print('=')
